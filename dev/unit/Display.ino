7a991ffe4fe564aceb54108bbb285a99033c77a7
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2019-01-13 12:12:01 -0500

    Update Display.ino

--- dev/unit/Display.ino
+++ dev/unit/Display.ino
@@ -1,7 +1,9 @@
 #include <Display.h>
 #include <Score.h>
+#include <BallCount.h>
 
 int pin2[] = {2,0,0,0,0};
+int pin3 = 3;
 const int pin7 = 7;
 const int pin8 = 8;
 const int pin9 = 9;
@@ -13,22 +15,31 @@ const int pin13 = 13;
 //int pinsHP[] = {pin13,pin12, pin11, pin10, pin9, pin8, pin7};
 int pinsHP[] = {0,0, 0, 0, 0, 0, 0};
 
-int pinsTP[] = {pin13,pin12, pin11, pin10, pin9, pin8, pin7};
-//int pinsTP[] = {0,0, 0, 0, 0, 0, 0};
+//int pinsTP[] = {pin13,pin12, pin11, pin10, pin9, pin8, pin7};
+int pinsTP[] = {0,0, 0, 0, 0, 0, 0};
 
 //int pinsOP[] = {pin13,pin12, pin11, pin10, pin9, pin8, pin7};
 int pinsOP[] = {0,0, 0, 0, 0, 0, 0};
 
+int pinsBC[] = {pin13,pin12, pin11, pin10, pin9, pin8, pin7};
+
 Score score;
+BallCount ballCount;
 Display display;
 
 void setup() {
   score.SetScoreSwitchPins(pin2,1);
   display.SetScoreDisplayPins(pinsHP,pinsTP,pinsOP);
+
+  ballCount.SetBallCountSwitchPin(pin3);
+  display.SetBallCountDisplayPins(pinsBC);
   Serial.begin(9600);
 }
 
 void loop() {
   Serial.println(score.GetScore());
   display.DisplayScore(score.GetScore());
+
+  Serial.println(ballCount.GetBallCount());
+  display.DisplayBallCount(ballCount.GetBallCount());
 }
